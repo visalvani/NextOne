d3c5558c9833e5ce9b83323c5959f3c41c83feef
Raghavendran Ramamoorthy <vs.visalakshi@gmail.com>
2019-02-27 10:03:22 +0530

    Next

---
+++ src/org/test/TestGIt.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class TestGIt {
+
+
+public void EmpId() {
+	System.out.println("EnterYour Name");
+}
+public static void main(String[] args) {
+	
+	TestGIt emp = new TestGIt();
+	emp.EmpId();
+}
+	
+	
+}
